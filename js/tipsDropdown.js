4c6522ee85129f8c25ef33407cd377ff0a95b155
Oliver Christensen <orc13a@live.dk>
2021-01-26 16:05:56 +0100

    added product to page

--- js/tipsDropdown.js
+++ js/tipsDropdown.js
@@ -1,24 +1,17 @@
-function dropdown(elt) {
-    var dropdownCheck = elt.children[0].children[0];
-    var iconClosed = elt.children[0].children[2].children[0];
-    var iconOpen = elt.children[0].children[2].children[1];
-    var contentContainer = elt.children[1];
+function dropdown(elt, contentDiv) {
+    var closedIcon = elt.children[1].children[0];
+    var openIcon = elt.children[1].children[1];
+    var content = document.getElementById(contentDiv);
 
-    var counter = 0;
+    if (content.style.display === 'none') {
+        content.style.display = 'block';
+        
+        closedIcon.style.display = 'none';
+        openIcon.style.display = 'inline-block';
+    } else {
+        content.style.display = 'none';
 
-    if (dropdownCheck.checked === false && counter === 0) {
-        dropdownCheck.checked = true;
-        iconClosed.style.display = 'none';
-        iconOpen.style.display = 'inline-block';
-        contentContainer.style.display = 'block';
-        counter++;
-    }
-    
-    if (dropdownCheck.checked === true && counter === 0) {
-        dropdownCheck.checked = false;
-        iconClosed.style.display = 'inline-block';
-        iconOpen.style.display = 'none';
-        contentContainer.style.display = 'none';
-        counter++;
+        closedIcon.style.display = 'inline-block';
+        openIcon.style.display = 'none';
     }
 }
